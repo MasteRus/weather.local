ac0ac9b4849defc7b8a65c0785aa967978b1f59a
rkasatkin <kasatkin@azoft.com>
2023-08-08 14:20:14 +0600

    feature-003-DataSource interface

---
+++ app/Service/WeatherDataSource/WeatherSourceInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Service\WeatherDataSource;
+
+use App\Models\Location;
+
+interface WeatherSourceInterface
+{
+    public function getData(string $startDate, string $finishDate, Location $location);
+}
