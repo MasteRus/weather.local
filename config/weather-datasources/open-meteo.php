1e938f4bed9ad68575deaa379a8ae713cddab6a3
rkasatkin <kasatkin@azoft.com>
2023-08-10 18:06:29 +0600

    feature-011 - Documentation, config description, README.md

--- config/weather-datasources/open-meteo.php
+++ config/weather-datasources/open-meteo.php
@@ -1,11 +1,13 @@
 <?php
 
 return [
+    # Включает или отключает сбор с этого источника
     'enabled'        => true,
     'historical_url' => env('DATA_SOURCE_OPEN_METEO_HISTORICAL', 'https://archive-api.open-meteo.com/v1/archive'),
+    # Временно не используется
     'forecast_url'   => env('DATA_SOURCE_OPEN_METEO_FORECAST', 'https://api.open-meteo.com/v1/forecast'),
     'timezone'       => 'GMT',
-    // Название слоя в источнике - тип слоя у нас
+    // Название слоя в источнике - тип слоя у нас, группировка данных из разных источников производится по типу слоя
     'layers'         => [
         'temperature_2m_max'  => 'temperature_max',
         'temperature_2m_min'  => 'temperature_min',
--- config/weather-datasources/weather-api.php
+++ config/weather-datasources/weather-api.php
@@ -1,11 +1,13 @@
 <?php
 
 return [
+    # Включает или отключает сбор с этого источника
     'enabled'      => true,
     'apikey'       => env('DATA_SOURCE_WEATHER_API_KEY', ''),
+    # Временно не используется
     'history_url'  => env('DATA_SOURCE_WEATHERAPI_HISTORICAL', 'http://api.weatherapi.com/v1/history.json'),
     'forecast_url' => env('DATA_SOURCE_WEATHERAPI_FORECAST', 'http://api.weatherapi.com/v1/forecast.json'),
-    // Название слоя в источнике - тип слоя у нас
+    // Название слоя в источнике - тип слоя у нас, группировка данных из разных источников производится по типу слоя
     'layers'       => [
         'avgtemp_c'      => 'temperature',
         'mintemp_c'      => 'temperature_min',
